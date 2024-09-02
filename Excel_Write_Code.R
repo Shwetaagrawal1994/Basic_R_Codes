4546b0545bb816357dcbec826ead387ba4a77ae1
Shwetaagrawal1994 <shwetaagra20@gmail.com>
2024-09-02 20:08:19 +0530

    Excel_Write_Code.R

--- Excel_Write_Code.R
+++ Excel_Write_Code.R
@@ -6,7 +6,7 @@
 
 packages <- c("openxlsx")
 installed_packages <- packages %in% rownames(installed.packages() )
-if ( any(installed_packages == FALSE) ) {
+if (any(installed_packages == FALSE)) {
    installed.packages( packages[!installed_packages] )
 }
 
@@ -16,19 +16,20 @@ invisible(lapply(packages, library, character.only = TRUE))
 ###########################
 # DEFINE PATH
 ###########################
-setwd( dirname(rstudioapni::getSourceEditorContext()$path) )
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 setwd('..')
 input_Path = paste0( getwd(), "/Input/")
 
 Data = data.frame( Name = c('shweta', 'Vanishika', 'Shreya', 'Shristhi' ), Number = c(100, 200, 300, 400) )
 
 wb2 <- loadWorkbook( paste0(input_Path, "Trial.xlsx") )
-writeData(wb2, "TrialTab", Data, startCol = 1, startRow = 1, rownames = F)
+writeData(wb2, "trialTab", Data, startCol = 1, startRow = 1, rownames = F)
 
 protectWorksheet(wb2,
-                protect = TRUE,
-                password = "ABC",
-                loackFormattingCells = TRUE,
-                loackFormattingColumns = TRUE)
+               "trialTab",
+               protect = TRUE,
+               password = "ABC",
+               lockFormattingCells = TRUE,
+               lockFormattingColumns = TRUE)
 
 saveWorkbook(wb2, paste0(input_Path, "Trial.xlsx"), overwrite = TRUE )
